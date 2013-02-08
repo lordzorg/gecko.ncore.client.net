40b7384b01a4ccde5248bcc55e985ffb391565ca
Kim Ommundsen <ko@gecko.no>
2013-02-08 13:14:24 +0000

    Build Fix ***NO_CI***
    git-tfs-id: [http://tfs.gecko.no:8080/tfs/DefaultCollection]$/Projects/NCoreServices/Client/2013.01;C17869

---
+++ net35/Client.Functions.V1/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ePhorte® Integration Services Functions Service V1 Proxy")]
+[assembly: AssemblyDescription("A client proxy that can be used to access service.")]
+[assembly: AssemblyConfiguration("")]
+
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f7c52f83-1b66-4eab-b47a-6c2fc583642b")]
+
---
+++ net35/Client.Metadata.V1/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ePhorte® Integration Services Metadata Service V1 Proxy")]
+[assembly: AssemblyDescription("A client proxy that can be used to access service.")]
+[assembly: AssemblyConfiguration("")]
+
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2a4d29d1-784f-4637-be56-00e74ebc68ea")]
