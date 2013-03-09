4f8e0dc3fafc379601d6360fdefc6c5398b1dc85
Steinar Herland <she@gecko.no>
2013-03-09 14:41:31 +0000

    * Legge til Ephorte.ServiceModel.Client.ObjectModel.V3.En - Klient som bruker OMS DTOene * Legge til Pack.bat som gjører release-build og deretter pack.msbuild ***NO_CI***
    git-tfs-id: [http://tfs.gecko.no:8080/tfs/DefaultCollection]$/Projects/NCoreServices/Client/2013.01;C18196

---
+++ net45/ServiceModel.Client.ObjectModel.V3.En/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ePhorte® Integration Services Object Model Service V3.En Proxy using WCF service contract objects")]
+[assembly: AssemblyDescription("A client proxy that can be used to access service.")]
+[assembly: AssemblyConfiguration("")]
+
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("21c00434-3f05-4645-8e1d-9394789a79fe")]
+
+//[assembly: AllowPartiallyTrustedCallers]
+// Version information for an assembly consists of the following three values (according to SemVer schema) :
+//      Major Version
+//      Minor Version 
+//      Patch
+// You can specify all the values or you can default the Patche Numbers by using the '*'.
+
+// Dette versjonsnummeret beholdes konstant og endres aldri
+[assembly: AssemblyVersion("1.0.0")]
+
+// Dette versjonsnummeret økes i hennhold til semver.org
+[assembly: AssemblyFileVersion("0.0.1.*")]
+
+// Dette versjonsnummeret bør samsvare med AssemblyFileVersion pluss eventuelt postfiks for pre release pakker.
+[assembly: AssemblyInformationalVersion("0.0.1-Alfa1")]
