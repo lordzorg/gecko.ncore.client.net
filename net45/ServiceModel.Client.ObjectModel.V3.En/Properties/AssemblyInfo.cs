c14eaec8f99a551e7c09de993f663e9710501441
Steinar Herland <she@gecko.no>
2013-09-04 11:30:52 +0200

    - Remove direct dependencies to WCFExtrasPlus - Update OMS v3 [en|no]

--- net45/ServiceModel.Client.ObjectModel.V3.En/Properties/AssemblyInfo.cs
+++ net45/ServiceModel.Client.ObjectModel.V3.En/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("6.2.0")]
 
 // Dette versjonsnummeret økes i hennhold til semver.org
-[assembly: AssemblyFileVersion("0.0.5")]
+[assembly: AssemblyFileVersion("1.0.0")]
 
 // Dette versjonsnummeret bør samsvare med AssemblyFileVersion pluss eventuelt postfiks for pre release pakker.
-[assembly: AssemblyInformationalVersion("0.0.5-Alfa")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
